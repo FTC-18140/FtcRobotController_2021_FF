20c458c119542c35ff49038c8fabd39b8d75aca2
Chris <mobilechrisd@gmail.com>
2021-09-24 15:24:33 -0500

    Created clean Thunderbot class called Thunderbox 2021

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Thunderbot_2021.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
+
+public class Thunderbot_2021 {
+    /**
+     * Public OpMode members
+     */
+    DcMotor leftFront = null;
+    DcMotor rightFront = null;
+    DcMotor leftRear = null;
+    DcMotor rightRear = null;
+
+
+    /**
+     * local OpMode members
+     */
+    HardwareMap hwMap = null;
+    private Telemetry telemetry;
+
+    /**
+     * Constructor
+     */
+    public Thunderbot_2021() {
+
+    }
+
+    /**
+     * Initialize standard Hardware interfaces
+     */
+    public void init(HardwareMap ahwMap, Telemetry telem) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        telemetry = telem;
+
+        // Define & Initialize Motors
+        rightFront = hwMap.dcMotor.get("rightFront");
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rightRear = hwMap.dcMotor.get("rightRear");
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront = hwMap.dcMotor.get("leftFront");
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftRear = hwMap.dcMotor.get("leftRear");
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRear.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    // Stop all motors
+    public void stop() {
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftRear.setPower(0);
+        rightRear.setPower(0);
+    }
+}
